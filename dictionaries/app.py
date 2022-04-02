f44b8f81e2c9577580eee2256ec39f9cb68afada
Hanna Söderström <gothbarbie84@gmail.com>
2022-04-02 13:09:11 +0200

    feat: add student

--- dictionaries/app.py
+++ dictionaries/app.py
@@ -1,5 +1,22 @@
 # Dictionary: key-value pairs
+sample_dictionary = {
+  "name": "Hanna", 
+  "age": 38, 
+  "pets": ["Xena", "Atlas", "Zeus"], 
+  "numbers": [1, 2, 3, 4], 
+  "spouse": { 
+    "Issy": { "age": 35 } 
+  } 
+}
 
-sample_dictionary = {"name": "Hanna", "age": 38, "pets": ["Xena", "Atlas", "Zeus"], "numbers": [1, 2, 3, 4], "spouse": { "Issy": { "age": 35 } } }
+#print(sample_dictionary)
 
-print(sample_dictionary)
+def create_student():
+  name = input("Enter the student's name: ")
+
+  student = { 'name': name, 'marks': [] }
+
+  return student
+
+
+print(create_student())
