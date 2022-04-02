13adbe9a2552d00890a12269fa03a16f6cf5c720
Hanna Söderström <gothbarbie84@gmail.com>
2022-04-02 12:38:37 +0200

    feat: finish lottery

--- number.py
+++ number.py
@@ -1,13 +1,23 @@
 import random
 
-# Get users guesses
+def menu():
+  # Get users guesses
+  user_numbers = get_player_numbers()
+
+  # Generate 6 random numbers (between 1-10)
+  lottery_numbers = create_lottery_numbers()
+
+  # Match user and lottery numbers
+  matched_numbers = user_numbers.intersection(lottery_numbers)
+  # Print winnings
+  print("You matched {}. You won €{}".format(matched_numbers, 10 ** len(matched_numbers)))
+  
 def get_player_numbers():
   number_csv = input("Enter 6 numbers, separated by commas: ")
   number_list = number_csv.split(",")
   integer_set = { int(number) for number in number_list }
   return integer_set
 
-# Generate 6 random numbers (between 1-10)
 def create_lottery_numbers(): 
   values = set()
   
@@ -16,10 +26,5 @@ def create_lottery_numbers():
   
   return values
 
-# Match user and lottery numbers
 
-
-
-# Calculate winnings based on numbers matched
-
-print(create_lottery_numbers())
+menu()
