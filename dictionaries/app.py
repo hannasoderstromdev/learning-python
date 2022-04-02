8a5bddfb522bec8827227bacbcef95849fde55a5
Hanna Söderström <gothbarbie84@gmail.com>
2022-04-02 13:01:24 +0200

    feat: add dictionary

---
+++ dictionaries/app.py
@@ -0,0 +1,5 @@
+# Dictionary: key-value pairs
+
+sample_dictionary = {"name": "Hanna", "age": 38, "pets": ["Xena", "Atlas", "Zeus"], "numbers": [1, 2, 3, 4], "spouse": { "Issy": { "age": 35 } } }
+
+print(sample_dictionary)
